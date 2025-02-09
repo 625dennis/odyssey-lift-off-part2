736071a3919ef00c69086f6f6536d28abb436c3d
Dennis Kuduzovic <dkuduzovic@auditboard.com>
2025-02-08 22:01:46 -0700

    the shape of a resolver

---
+++ server/src/resolvers.ts
@@ -0,0 +1,9 @@
+export const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate
+    // the homepage grid of our web client
+    tracksForHome: (parent, args, contextValue, info) => {},
+  },
+};
+
+export default resolvers;
